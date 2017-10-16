eb12693ec10277c80d74e24939f6a8faca4c0c45
steaphan <xifunlei@qq.com>
2017-10-16 18:29:15 +0800

    upload repository
    upload repository

---
+++ ESExport/Ribbon1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace ESExport
+{
+    partial class Ribbon1 : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public Ribbon1()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.group1 = this.Factory.CreateRibbonGroup();
+            this.button1 = this.Factory.CreateRibbonButton();
+            this.label1 = this.Factory.CreateRibbonLabel();
+            this.label2 = this.Factory.CreateRibbonLabel();
+            this.button2 = this.Factory.CreateRibbonButton();
+            this.label3 = this.Factory.CreateRibbonLabel();
+            this.label4 = this.Factory.CreateRibbonLabel();
+            this.button3 = this.Factory.CreateRibbonButton();
+            this.tab1.SuspendLayout();
+            this.group1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.Groups.Add(this.group1);
+            this.tab1.Label = "TabAddIns";
+            this.tab1.Name = "tab1";
+            // 
+            // group1
+            // 
+            this.group1.Items.Add(this.button1);
+            this.group1.Items.Add(this.label1);
+            this.group1.Items.Add(this.label2);
+            this.group1.Items.Add(this.button2);
+            this.group1.Items.Add(this.label3);
+            this.group1.Items.Add(this.label4);
+            this.group1.Items.Add(this.button3);
+            this.group1.Label = "自定义导出";
+            this.group1.Name = "group1";
+            // 
+            // button1
+            // 
+            this.button1.Image = global::ESExport.Properties.Resources.ico;
+            this.button1.Label = "C#导出";
+            this.button1.Name = "button1";
+            this.button1.ShowImage = true;
+            this.button1.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Label = " ";
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            this.label2.Label = " ";
+            this.label2.Name = "label2";
+            // 
+            // button2
+            // 
+            this.button2.Image = global::ESExport.Properties.Resources.ico;
+            this.button2.Label = "Csv导出";
+            this.button2.Name = "button2";
+            this.button2.ShowImage = true;
+            this.button2.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.Label = " ";
+            this.label3.Name = "label3";
+            // 
+            // label4
+            // 
+            this.label4.Label = " ";
+            this.label4.Name = "label4";
+            // 
+            // button3
+            // 
+            this.button3.Image = global::ESExport.Properties.Resources.ico;
+            this.button3.Label = "目录设置";
+            this.button3.Name = "button3";
+            this.button3.ShowImage = true;
+            this.button3.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button3_Click);
+            // 
+            // Ribbon1
+            // 
+            this.Name = "Ribbon1";
+            this.RibbonType = "Microsoft.Excel.Workbook";
+            this.Tabs.Add(this.tab1);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Ribbon1_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.group1.ResumeLayout(false);
+            this.group1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonLabel label1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonLabel label2;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button2;
+        internal Microsoft.Office.Tools.Ribbon.RibbonLabel label3;
+        internal Microsoft.Office.Tools.Ribbon.RibbonLabel label4;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button3;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal Ribbon1 Ribbon1
+        {
+            get { return this.GetRibbon<Ribbon1>(); }
+        }
+    }
+}
---
+++ ESExport/Ribbon1.cs
@@ -0,0 +1,344 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+using Microsoft.Win32;
+using System.Windows.Forms;
+using System.IO;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Text.RegularExpressions;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace ESExport
+{
+    public partial class Ribbon1
+    {
+        #region RegistStoreData
+        public static readonly string registryPath = @"Software\YourCompanyName\YourAddInName";
+
+        public static void StoreInRegistry(string keyName, string value)
+        {
+            RegistryKey rootKey = Registry.CurrentUser;
+            using (RegistryKey rk = rootKey.CreateSubKey(registryPath))
+            {
+                rk.SetValue(keyName, value, RegistryValueKind.String);
+            }
+        }
+
+        public static string ReadFromRegistry(string keyName, string defaultValue = "")
+        {
+            RegistryKey rootKey = Registry.CurrentUser;
+            using (RegistryKey rk = rootKey.OpenSubKey(registryPath, false))
+            {
+                if (rk == null)
+                {
+                    return defaultValue;
+                }
+
+                var res = rk.GetValue(keyName, defaultValue);
+                if (res == null)
+                {
+                    return defaultValue;
+                }
+
+                return res.ToString();
+            }
+        }
+        #endregion
+        public static readonly string EndSign = @"#End";
+        public static string exportPath = ReadFromRegistry("ExportPath");
+
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        void Save2File(string name, string data)
+        {
+            var path = Path.Combine(exportPath, name);
+            using (StreamWriter sw = File.CreateText(path))
+            {
+                sw.Write(data);
+            }
+            MessageBox.Show("导出文件: " + path);
+        }
+
+        /// <summary>
+        /// 检查有效数据区
+        /// </summary>
+        void CheckDataCount(Excel.Worksheet worksheet, out int rowCount, out int columnCount)
+        {
+            rowCount = worksheet.UsedRange.Rows.Count;
+            columnCount = worksheet.UsedRange.Columns.Count;
+            int k;
+            for (k = 1; k <= columnCount; k++)
+            {
+                var value = (worksheet.Cells[1, k] as Excel.Range).Value;
+                if (value == null)
+                {
+                    break;
+                }
+                string param_name = value.ToString();
+                param_name = param_name.Trim();
+                if (param_name == EndSign)
+                {
+                    break;
+                }
+                if (string.IsNullOrEmpty(param_name))
+                {
+                    break;
+                }
+            }
+            columnCount = k - 1;
+
+            for (k = 1; k <= rowCount; k++)
+            {
+                bool null_row = true;
+                for (int j = 1; j <= columnCount; j++)
+                {
+                    var value = (worksheet.Cells[k, j] as Excel.Range).Value;
+                    if (value == null)
+                    {
+                        continue;
+                    }
+                    string cell_var = value.ToString();
+                    if (j == 1 && cell_var == EndSign)
+                    {
+                        break;
+                    }
+                    if (!string.IsNullOrEmpty(cell_var.Trim()))
+                    {
+                        null_row = false;
+                        break;
+                    }
+                }
+                if (null_row)
+                {
+                    break;
+                }
+            }
+            rowCount = k - 1;
+        }
+
+
+        private void button1_Click(object sender, RibbonControlEventArgs e)
+        {
+            // export c#
+            Excel.Worksheet worksheet = (Excel.Worksheet)Globals.ThisAddIn.Application.ActiveSheet;
+            int rowCount, columnCount;
+            CheckDataCount(worksheet, out rowCount, out columnCount);
+            if (rowCount < 3)
+            {
+                MessageBox.Show("至少需要3行数据!（变量名,类型,数据)");
+                return;
+            }
+            string[] param_names = new string[columnCount];
+            string[] param_types = new string[columnCount];
+            for (int k = 1; k <= columnCount; k++)
+            {
+                var value1 = (worksheet.Cells[1, k] as Excel.Range).Value;
+                var value2 = (worksheet.Cells[2, k] as Excel.Range).Value;
+                if (value1 == null || value2 == null)
+                {
+                    MessageBox.Show("前两行定义不能有空!");
+                    return;
+                }
+                string param_name = value1.ToString();
+                param_name = param_name.Trim();
+                string param_type = value2.ToString();
+                param_type = param_type.Trim();
+                if (string.IsNullOrEmpty(param_name) || string.IsNullOrEmpty(param_type))
+                {
+                    MessageBox.Show("前两行定义不能有空!");
+                    return;
+                }
+                param_names[k - 1] = param_name;
+                param_types[k - 1] = param_type;
+            }
+
+            string s = System.Configuration.ConfigurationManager.AppSettings.Get("template");
+            //去注释
+            Regex regex = new Regex("//[^\\r\\n]*[\\r\\n]+");
+            var match = regex.Match(s);
+            while (match.Success)
+            {
+                s = s.Replace(match.Value, "");
+                match = regex.Match(s);
+            }
+            // 替换表格名字
+            string clsName = worksheet.Name;
+            regex = new Regex(@"#SHEET_NAME#");
+            match = regex.Match(s);
+            while (match.Success)
+            {
+                s = s.Replace(match.Value, clsName);
+                match = regex.Match(s);
+            }
+
+            // 参数定义
+            regex = new Regex("#PARAM_DEF#.*#PARAM_END#", RegexOptions.Singleline);
+            match = regex.Match(s);
+            if (match.Success)
+            {
+                string formatStr = match.Value.Replace("#PARAM_DEF#", "");
+                formatStr = formatStr.Replace("#PARAM_END#", "").Trim();
+
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < columnCount; i++)
+                {
+                    sb.AppendFormat(formatStr, param_types[i], param_names[i]);
+                    if (i != columnCount - 1)
+                    {
+                        sb.AppendLine();
+                    }
+                }
+                s = s.Replace(match.Value, sb.ToString());
+            }
+            else
+            {
+                MessageBox.Show("#PARAM_DEF# 匹配失败！");
+                return;
+            }
+
+            // 数据定义
+            regex = new Regex("#DATA_DEF#.*#DATA_END#", RegexOptions.Singleline);
+            match = regex.Match(s);
+            if (match.Success)
+            {
+                string matchAllStr = match.Value;
+                string formatStr = matchAllStr.Replace("#DATA_DEF#", "");
+                formatStr = formatStr.Replace("#DATA_END#", "").Trim();
+
+                regex = new Regex("#PAIR_DEF#.*#PAIR_END#", RegexOptions.Singleline);
+                match = regex.Match(formatStr);
+                if (match.Success)
+                {
+                    string pairStr = match.Value;
+                    string formatStr2 = pairStr.Replace("#PAIR_DEF#", "");
+                    formatStr2 = formatStr2.Replace("#PAIR_END#", "").Trim();
+
+
+                    regex = new Regex(@"string");
+                    StringBuilder sb = new StringBuilder();
+                    StringBuilder sb2 = new StringBuilder();
+                    for (int i = 3; i <= rowCount; i++)
+                    {
+                        sb2.Clear();
+                        for (int j = 1; j <= columnCount; j++)
+                        {
+
+                            string stype = param_types[j - 1];
+                            string sname = param_names[j - 1];
+                            bool strflg = regex.Match(stype).Success;
+
+                            var value = (worksheet.Cells[i, j] as Excel.Range).Value;
+                            bool nullflg = value == null;
+                            if (!nullflg)
+                            {
+                                string vstr = value.ToString();
+                                vstr = vstr.Trim();
+                                if (string.IsNullOrEmpty(vstr))
+                                {
+                                    nullflg = true;
+                                }
+                                else
+                                {
+                                    if (strflg)
+                                    {
+                                        vstr = string.Format("\"{0}\"", vstr);
+                                    }
+                                    sb2.AppendFormat(formatStr2, sname, vstr);
+                                }
+                            }
+                            if (nullflg)
+                            {
+                                // null cell
+                                if (strflg)
+                                {
+                                    // string type?
+                                    sb2.AppendFormat(formatStr2, sname, "\"\"");
+                                }
+                                else
+                                {
+                                    sb2.AppendFormat(formatStr2, sname, 0);
+                                }
+                            }
+                            if (j != columnCount)
+                            {
+                                sb2.Append(',');
+                            }
+                        }
+
+                        sb.Append(formatStr.Replace(pairStr, sb2.ToString()));
+                        if (i != rowCount)
+                        {
+                            sb.AppendLine();
+                        }
+                    }
+                    s = s.Replace(matchAllStr, sb.ToString());
+                }
+                else
+                {
+                    MessageBox.Show("#PAIR_DEF# 匹配失败！");
+                    return;
+                }
+            }
+            else
+            {
+                MessageBox.Show("#DATA_DEF# 匹配失败！");
+                return;
+            }
+            Save2File(worksheet.Name + ".cs", s);
+        }
+
+        private void button2_Click(object sender, RibbonControlEventArgs e)
+        {
+            // export csv
+            Excel.Worksheet worksheet = (Excel.Worksheet)Globals.ThisAddIn.Application.ActiveSheet;
+            int rowCount, columnCount;
+            CheckDataCount(worksheet, out rowCount, out columnCount);
+            if (rowCount < 3)
+            {
+                MessageBox.Show("至少需要3行数据!（变量名,类型,数据)");
+                return;
+            }
+            StringBuilder sb = new StringBuilder();
+            for (int i = 1; i <= rowCount; i++)
+            {
+                Excel.Range range = worksheet.UsedRange.Rows[i];
+                for (int j = 1; j <= columnCount; j++)
+                {
+                    Excel.Range cell = range.Columns[j];
+                    if (cell.Value != null)
+                    {
+                        sb.Append(cell.Value.ToString().Trim());
+                    }
+                    if (j != columnCount)
+                    {
+                        sb.Append(',');
+                    }
+                }
+                if (i != rowCount)
+                {
+                    sb.Append("\t\n");
+                }
+            }
+            Save2File(worksheet.Name + ".csv", sb.ToString());
+        }
+
+        private void button3_Click(object sender, RibbonControlEventArgs e)
+        {
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.InitialDirectory = exportPath;
+            dialog.IsFolderPicker = true;
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                exportPath = dialog.FileName;
+                StoreInRegistry("ExportPath", exportPath);
+
+                MessageBox.Show("设置工作目录: " + exportPath);
+            }
+        }
+    }
+}
